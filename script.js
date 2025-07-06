d02ec9bcdf22db4424a4eaf511773c0610b0d138
sarihalhal22 <amein3amin@gmail.com>
2025-07-06 17:29:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-document.getElementById('trustBtn').addEventListener('click', function () {
-    const music = document.getElementById('music');
-    document.getElementById('surprise').classList.remove('hidden');
-    this.style.display = 'none';
+document.getElementById("trustBtn").addEventListener("click", function () {
+  const trustBtn = this;
+  const loading = document.getElementById("loading");
+  const surprise = document.getElementById("surprise");
+  const music = document.getElementById("music");
 
-    // Try playing music after user gesture
-    music.play().catch(function(error) {
-        console.log("Audio failed to play:", error);
+  trustBtn.style.display = "none";
+  loading.classList.remove("hidden");
+
+  setTimeout(() => {
+    loading.classList.add("hidden");
+    surprise.classList.remove("hidden");
+
+    // Change the source of audio to Rick Astley's Never Gonna Give You Up
+    music.src =
+      "https://cdn.jsdelivr.net/gh/iqasnimr/music-cdn@master/Rick_Astley_-_Never_Gonna_Give_You_Up.mp3";
+
+    music.play().catch((e) => {
+      console.log("Audio play blocked:", e);
     });
+  }, 2500); // fake loading delay
 });
+
 
