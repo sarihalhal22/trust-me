759f1d6ba9dd26d40bf4e14647f5e816e42ec5c1
sarihalhal22 <amein3amin@gmail.com>
2025-07-07 11:35:30 +0300

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-
-document.getElementById('trustBtn').addEventListener('click', function() {
-    document.getElementById('surprise').classList.remove('hidden');
-    this.style.display = 'none';
-});
